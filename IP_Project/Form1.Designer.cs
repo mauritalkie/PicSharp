0f1b3b059f9c197636d272a6dd709df629373bf1
mauritalkie <93022536+mauritalkie@users.noreply.github.com>
2023-06-08 17:00:04 -0600

    camera activation

--- IP_Project/Form1.Designer.cs
+++ IP_Project/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            cbCamera = new ComboBox();
+            label1 = new Label();
+            pbDisplay = new PictureBox();
+            btnStart = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbDisplay).BeginInit();
+            SuspendLayout();
+            // 
+            // cbCamera
+            // 
+            cbCamera.FormattingEnabled = true;
+            cbCamera.Location = new Point(150, 12);
+            cbCamera.Name = "cbCamera";
+            cbCamera.Size = new Size(334, 33);
+            cbCamera.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Camera:";
+            // 
+            // pbDisplay
+            // 
+            pbDisplay.Location = new Point(12, 51);
+            pbDisplay.Name = "pbDisplay";
+            pbDisplay.Size = new Size(593, 347);
+            pbDisplay.TabIndex = 2;
+            pbDisplay.TabStop = false;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(493, 404);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(112, 34);
+            btnStart.TabIndex = 3;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(618, 450);
+            Controls.Add(btnStart);
+            Controls.Add(pbDisplay);
+            Controls.Add(label1);
+            Controls.Add(cbCamera);
+            Name = "Form1";
+            Text = "Webcam";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pbDisplay).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox cbCamera;
+        private Label label1;
+        private PictureBox pbDisplay;
+        private Button btnStart;
     }
 }
--- IP_Project/Form1.cs
+++ IP_Project/Form1.cs
@@ -1,10 +1,50 @@
+using AForge.Video;
+using AForge.Video.DirectShow;
+
 namespace IP_Project
 {
     public partial class Form1 : Form
     {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        bool isCameraActivated = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (isCameraActivated) return;
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cbCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+            isCameraActivated = true;
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pbDisplay.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cbCamera.Items.Add(filterInfo.Name);
+            cbCamera.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning == true)
+            {
+                videoCaptureDevice.SignalToStop();
+                videoCaptureDevice = null;
+                isCameraActivated = false;
+            }
         }
     }
 }
